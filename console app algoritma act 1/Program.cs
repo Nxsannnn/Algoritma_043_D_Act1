e4f8087decde6fdc2abbf8822de319a8778c101b
Nxsannnn <ichsanalmunawwarg@gmail.com>
2022-10-03 21:54:23 +0700

    Code New

--- console app algoritma act 1/Program.cs
+++ console app algoritma act 1/Program.cs
@@ -8,10 +8,68 @@ namespace Algoritma_Act1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private int[] a = new int[20];
+        private int n;
+        public void read ()
         {
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya element pada Array:  ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Masukkan element array");
+            Console.WriteLine("----------------");
+            for (int i= 0; i < n; i++)
+            {
+                Console.WriteLine("<" + (i+1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
+        public void display ()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Element array yang telah tersusun");
+            Console.WriteLine("----------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
 
         }
-        int a;
+        public void BubbleSortArray()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program mylist = new Program();
+            mylist.read();
+            mylist.BubbleSortArray();
+            mylist.display();
+            Console.WriteLine("\n\nTekan Tombol Apa Saja Untuk Keluar.");
+            Console.Read();
+        }
     }
 }
